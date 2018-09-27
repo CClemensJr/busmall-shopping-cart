479eb2e24f999c541d7989da943bc4c5e648450c
Charles Clemens <cclemensjr@gmail.com>
2018-09-27 15:53:51 -0700

    Implemented quantity of items

--- js/catalog.js
+++ js/catalog.js
@@ -56,8 +56,11 @@ function addSelectedItemToCart() {
   var selectedItem = selected.value;
   console.log(selectedItem);
 
-
   // TODO: get the quantity
+  var qty = document.getElementById('quantity');
+  var itemQty = qty.value;
+  console.log(itemQty);
+  
   // TODO: using those, add one item to the Cart
 }
 
